0e79116565e09889081dc87d2ecdb57b82ac7f86
shane mendez <shane@shanemendez.com>
2020-05-19 02:26:48 -0500

    reworked the error detection

--- src/tools/GitHelpers.js
+++ src/tools/GitHelpers.js
@@ -1,13 +1,21 @@
 let Git = require('nodegit')
 let path = require('path')
+
 module.exports = {
     async trackRemoteBranch(repo, remote_name, branch_name) {
 
         let remote_path = path.join(remote_name, branch_name)
 
         let branch_commit = await repo.getBranchCommit(remote_path)
-        let branch_ref = await repo.createBranch(branch_name, branch_commit)
-        await Branch.setUpstream(branch_ref, remote_path)
+        let branch_ref;
+        try{
+            branch_ref = await repo.createBranch(branch_name, branch_commit)
+        }
+        catch(err){
+            if(err.errno != -4) throw err
+            branch_ref = await repo.getBranch(branch_name)
+        }
+        await Git.Branch.setUpstream(branch_ref, remote_path)
         return true
 
     },
@@ -20,7 +28,7 @@ module.exports = {
      *  Todo: find more universal solution, 
      *  Not sure if this is always the ref name structure
      */
-    async cleaveRef(remote_ref) {
+    cleaveRef(remote_ref) {
         let parts = remote_ref.split('/')
         let [remote_name, branch_name] = parts.slice(parts.length - 2)
         return {
@@ -32,7 +40,6 @@ module.exports = {
         let refs = await repo.getReferences()
         let remote_refs = refs
             .filter((ref) => ref.isRemote())
-            .map(ref => ref.name())
         return remote_refs
     },
     /**
@@ -40,11 +47,15 @@ module.exports = {
      */
     async trackAll(repo) {
         (await this.getRemoteReferences(repo))
+            .map(ref => ref.name())
             .map(ref => this.cleaveRef(ref))
             .forEach(async ({remote_name, branch_name}) => {
                 try {
+                    debugger
                     await this.trackRemoteBranch(repo, remote_name, branch_name)
-                } catch (err) {}
+                } catch (err) {
+                    console.log(err)
+                }
             })
         return true
     }
